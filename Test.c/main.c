5ddc75382a2caf2dfded5447f7a7c6914b37f226
Waliur Rahman <waliurrahaman450@gmail.com>
2022-10-20 09:51:17 +0600

    Initial Commit

---
+++ Test.c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Test.c
+//
+//  Created by Waliur Rahman on 20/10/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
